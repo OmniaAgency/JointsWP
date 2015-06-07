2b73368a4b731411371dfc9058baebd330bbf201
Jeremy Englert <jeremy@cmsadgroup.com>
2015-06-07 15:49:09 -0700

    Updated comments to match WP standards

--- assets/functions/comments.php
+++ assets/functions/comments.php
@@ -6,13 +6,6 @@ function joints_comments($comment, $args, $depth) {
 		<article id="comment-<?php comment_ID(); ?>" class="clearfix large-12 columns">
 			<header class="comment-author">
 				<?php
-				/*
-					This is the new responsive optimized comment image. It used the new HTML5 data-attribute to display comment gravatars on larger screens only. What this means is that on larger posts, mobile sites don't have a ton of requests for comment images. This makes load time incredibly fast! If you'd like to change it back, just replace it with the regular wordpress gravatar call:
-					echo get_avatar($comment,$size='32',$default='<path_to_url>' );
-				*/
-				?>
-				<!-- custom gravatar call -->
-				<?php
 					// create variable
 					$bgauthemail = get_comment_author_email();
 				?>
