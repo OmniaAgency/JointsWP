f9726c853ea0a768ad8058347bb6cf4efd21effb
Jeremy Englert <jeremy@cmsadgroup.com>
2015-06-09 23:54:06 -0700

    Fixed div alignment

--- footer.php
+++ footer.php
@@ -2,10 +2,10 @@
 						<div id="inner-footer" class="row">
 							<div class="large-12 medium-12 columns">
 								<nav role="navigation">
-		    					<?php joints_footer_links(); ?>
-		    				</nav>
-		    			</div>
-			        <div class="large-12 medium-12 columns">
+		    						<?php joints_footer_links(); ?>
+		    					</nav>
+		    				</div>
+							<div class="large-12 medium-12 columns">
 								<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
 							</div>
 						</div> <!-- end #inner-footer -->
